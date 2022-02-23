c9d852d86dacc789416a590a3d174881253209c5
zuenamash <mnachariazuenah@gmail.com>
2022-02-23 10:45:26 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,47 @@
+fun main(){
+    var y = ("house,animal")
+    println(y)
+    var s = ("sony,whitney")
+    println(s)
+    var a = ("anita,marrie")
+    println(a)
+    var m = ("34,13")
+    println(m)
+    var d = (100+200)
+    println(d)
+    var u = ("happy day")
+    println(u)
+    var p =multiply(6,7)
+    println(p)
+
+
+
+}
+fun photo(bb:String,cc:String):String{
+    var picture = "bb,cc"
+    return picture
+}
+fun girls(dd:String,ll:String):String{
+    var hello = "dd,ll"
+    return hello
+}
+fun akirachix(aa:String , mm:String):String{
+    var admin = "aa,mm"
+    return admin
+}
+fun numb(am:Int,ma:Int):Int{
+    var modul = am%ma
+    return modul
+}
+fun sum(pp:Int,sp:Int):Int{
+    var add = pp+sp
+    return add
+}
+fun fact(ss:String,pq:String):String{
+    var sun = ss + pq
+    return sun
+}
+fun multiply(xy:Int,cv:Int):Int{
+    var product = xy*cv
+    return product
+}
